80124ce9be4fc4ca3105eafcdfef414d16e9ce07
dinh dang <dangdd2@episerver.com>
2019-11-03 21:16:23 +0700

    add commerce

---
+++ src/EpiServerOrderPlugin/Infrastructure/OrderInitializationModule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using EPiServer.Framework;
+using EPiServer.Framework.Initialization;
+
+namespace EpiServerOrderPlugin.Infrastructure
+{
+    [InitializableModule]
+    [ModuleDependency(typeof(EPiServer.Commerce.Initialization.InitializationModule))]
+    public class OrderInitializationModule : IInitializableModule
+    {
+        public void Initialize(InitializationEngine context)
+        {
+            context.Locate.Advanced.GetInstance<OrderEventListener>().AddEvents();
+        }
+
+        public void Uninitialize(InitializationEngine context)
+        {
+            context.Locate.Advanced.GetInstance<OrderEventListener>().RemoveEvents();
+        }
+    }
+}
