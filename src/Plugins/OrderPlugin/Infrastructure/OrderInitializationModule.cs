c926c89a3b3d0a1251359ace59c9c302e09ac402
dinh dang <chinhtranv@outlook.com>
2019-11-04 02:14:18 +0700

    Upgrate Epi Fw to version 11.12

---
+++ src/Plugins/OrderPlugin/Infrastructure/OrderInitializationModule.cs
@@ -0,0 +1,20 @@
+﻿using EPiServer.Framework;
+using EPiServer.Framework.Initialization;
+
+namespace Dc.EpiServerOrderPlugin.Infrastructure
+{
+    [InitializableModule]
+    [ModuleDependency(typeof(EPiServer.Commerce.Initialization.InitializationModule))]
+    public class OrderInitializationModule : IInitializableModule
+    {
+        public void Initialize(InitializationEngine context)
+        {
+            context.Locate.Advanced.GetInstance<OrderEventListener>().AddEvents();
+        }
+
+        public void Uninitialize(InitializationEngine context)
+        {
+            context.Locate.Advanced.GetInstance<OrderEventListener>().RemoveEvents();
+        }
+    }
+}
